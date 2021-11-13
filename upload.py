5dc8146c8cfd88b90549043cb83a117206df25ee
An-GG <ankush.girotra@gmail.com>
2021-11-13 09:37:49 -0600

    logs

--- upload.py
+++ upload.py
@@ -8,22 +8,36 @@ def upload(port, baudrate, firmware_path):
     k_blocksize=512
     k_timeout_ms=30 * 1000
     k_target_fname="firmware.bin"
-    #
-    protocol = mbp.Protocol(port, baudrate, 512, k_timeout_ms)
+    print(1+"""
+SKR UPLOADER
+============
+DEV PORT:       %s
+BUADRATE:       %s
+FIRMWARE:       %s
+"""%(port, baudrate, firmware_path))
     
+    protocol = mbp.Protocol(port, baudrate, 512, k_timeout_ms)
     # INIT SD CARD
     protocol.send_ascii("M21") 
+
     # DELETE OLD .BIN FILE
+    print("""Removing existing '%s'..."""%k_target_fname)
     protocol.send_ascii("M30 "+k_target_fname)
     # STOP AUTO TEMP REPORTING
     protocol.send_ascii("M155 S0")
+    print("""Done. Preparing transfer session...""")
 
     protocol.connect()
+    print("MBTP Connect")
     transfer_session = mbp.FileTransferProtocol(protocol)
+    print("Flashing...")
     transfer_session.copy(firmware_path, k_target_fname, True, False)
+    print("Flashing successful.")
     protocol.disconnect()
+    print("MBTP Disconnect\nResetting...")
 
     os.system('echo M997 >> ' + port)
+    print("Done.")
 
 
 if __name__ == "__main__":
