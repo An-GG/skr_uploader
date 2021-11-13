cad2e31b515bec3154aacf946e5b5c05f2bab7e8
An-GG <ankush.girotra@gmail.com>
2021-11-13 11:56:57 -0600

    upload

--- upload.py
+++ upload.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python2
 import sys
 import os
+import time
 import binproto2 as mbp
 
 
 def upload(port, baudrate, firmware_path):
     k_blocksize=512
-    k_timeout_ms=30 * 1000
+    k_timeout_ms=1000
     k_target_fname="firmware.bin"
     print(1+"""
 SKR UPLOADER
@@ -16,7 +17,7 @@ BUADRATE:       %s
 FIRMWARE:       %s
 """%(port, baudrate, firmware_path))
     
-    protocol = mbp.Protocol(port, baudrate, 512, k_timeout_ms)
+    protocol = mbp.Protocol(port, baudrate, k_blocksize, k_timeout_ms)
     # INIT SD CARD
     protocol.send_ascii("M21") 
 
@@ -34,13 +35,30 @@ FIRMWARE:       %s
     transfer_session.copy(firmware_path, k_target_fname, True, False)
     print("Flashing successful.")
     protocol.disconnect()
-    print("MBTP Disconnect\nResetting...")
-
-    os.system('echo M997 >> ' + port)
+    print("MBTP Disconnect")
+    protocol.shutdown()
+    print("Session successfully shut down.")
+    reset(port, baudrate)
+    wait_for_reconnect(port, baudrate)
     print("Done.")
+
+def reset(port, baudrate):
+    os.system('stty -F ' + port + ' speed ' + str(baudrate) + ' -echo > /dev/null')
+    print("Device tty configured.")
+    os.system('echo M997 >> ' + port)
+    print("M997 RESET sent.")
+
+
+def wait_for_reconnect(port, baudrate):
+    print("Issuing M997 Reset...")
+    check_command = 'ls ' + port + ' > /dev/null 2>&1'
+    while(True):
+        time.sleep(1);
+        print(os.system(check_command))
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         sys.exit("USAGE: python %s [DEV_PORT] [BAUDRATE] [FIRMWARE_PATH]"%sys.argv[0]);
-    upload(sys.argv[1], sys.argv[2], sys.argv[3])
+    #upload(sys.argv[1], sys.argv[2], sys.argv[3])
+    wait_for_reconnect(sys.argv[1], sys.argv[2])
